4e67ba1b0218a31a68d7c4a5fc5b6cedbcb187b1
luanbelon <luansantana750@gmail.com>
2018-11-18 23:27:04 -0300

    Update randint-no-repeat.py

--- randint-no-repeat.py
+++ randint-no-repeat.py
@@ -1,46 +1,18 @@
 from random import randint
-import time
-'''c = {};
-for c in range(0,15):
-    print(randint(0,25))
-    time.sleep(3)'''
-
 
 lotofacil = []
-megasena = []
-quina = []
-lotomania = []
 
-
+#enquanto lotofacil for menor igual a 15
 while len(lotofacil) != 15:
+    
+    #r receberá um randint entre 1 e 25 (Numeros disponiveis para jogar na lotofacil)
     r = randint(1, 25)
-    '''time.sleep(2)'''
+    
+    #condicão se r nao estiver dentro de lotofacil
     if r not in lotofacil:
+        
+        #lotofacil acrescenta (.append) r a lista
         lotofacil.append(r)
+        
+#Por ultimo pede para imprimir lotofacil
 print(f'Lotofacil: {sorted(lotofacil)} \n')
-
-while len(megasena) != 6:
-    r = randint(0, 99)
-    '''time.sleep(2)'''
-    if r not in megasena:
-
-        megasena.append(r)
-print(f'Megasena: {sorted(megasena)} \n')
-
-while len(quina) != 5:
-    r = randint(1, 80)
-    '''time.sleep(2)'''
-    if r not in quina:
-
-        quina.append(r)
-print(f'Quina: {sorted(quina)} \n')
-
-#lotomania
-
-while len(lotomania) != 50:
-    r = randint(0, 99)
-    '''time.sleep(2)'''
-    if r not in lotomania:
-
-        lotomania.append(r)
-print(f'Lotomania: {sorted(lotomania)}')
